cf4ec674e8e19cf006959d9a02377c6eb0559ad2
phaltoe <reverse9@live.com>
2016-07-27 09:13:35 -0300

    created item model and associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ActiveRecord::Base
   validates_presence_of :name
   validates :name, length: {:minimum => 3}
+
+  has_many :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701131020) do
+ActiveRecord::Schema.define(version: 20160727121053) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "description"
+    t.boolean  "status"
+    t.integer  "priority"
+    t.integer  "list_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
